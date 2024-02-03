4f3b23798a8928c24236a9a7ea45f26ab263c476
Jayshil <jayshilpatel147@gmail.com>
2024-02-04 00:56:05 +0100

    data for a line

---
+++ Data/p1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+# This file is used to generate a synthetic data for a line
+m, c = 0.27, 2.7
+yerr = 0.03
+
+# Function to generate synthetic data
+def line(x, m, c):
+    return (m*x) + c
+
+x = np.sort(np.random.rand(20))
+y = np.array([np.random.normal(line(x[i], m, c), yerr) for i in range(20)])
+ye = np.abs(np.random.normal(0., yerr, 20))
+
+f1 = open(os.getcwd() + '/Data/line.dat', 'w')
+f1.write('#y = mx + c for m = ' + str(m) + ' and c = ' + str(c) + '\n')
+for i in range(20):
+    f1.write(str() + '\t' + str() + '\t' + str() + '\n')
+f1.close()
+
+plt.errorbar(x, y, yerr=ye, fmt='.')
+plt.plot(x, line(x, m, c), 'k-')
+plt.show()
